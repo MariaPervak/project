1af098e8d35874ffa016b9777e090a71e3532ffd
Мария Первак <mariia.pervak@irlix.ru>
2023-02-13 16:08:59 +0400

    stylelint, i18n plugin for eslint

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,27 +1,29 @@
-import {classNames} from "shared/lib/classNames/classNames";
+import { classNames } from 'shared/lib/classNames/classNames';
+import React, { useState } from 'react';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { LangSwitcher } from 'widgets/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
-import React, {useState} from "react";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
-import {LangSwitcher} from "widgets/LangSwitcher";
 
 interface SidebarProps{
     className?: string;
 }
 
-export const Sidebar = ({className}: SidebarProps) => {
+export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t, i18n } = useTranslation();
 
     const onToggle = () => {
-        setCollapsed(prevState => !prevState);
-    }
+        setCollapsed((prevState) => !prevState);
+    };
     return (
         <div
-            className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            <button onClick={onToggle}>toggle</button>
+            <button type="button" onClick={onToggle}>{t('переключить')}</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher className={cls.lang}/>
+                <LangSwitcher className={cls.lang} />
             </div>
         </div>
     );
